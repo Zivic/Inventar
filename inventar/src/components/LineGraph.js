2d6eb1a12f8c532af78fed0d5d589e0bff36888c
Djordje Zivic <djzivic@gmail.com>
2022-02-03 17:20:00 +0100

    Switched to simple opacity fill

--- inventar/src/components/LineGraph.js
+++ inventar/src/components/LineGraph.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useMemo } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import axios from "axios";
 import "chartjs-adapter-moment";
@@ -36,38 +36,6 @@ const LineGraph = (props) => {
     CategoryScale,
     TimeSeriesScale
   );
-
-  const MONTHS = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-  ];
-
-  function months(config) {
-    var cfg = config || {};
-    var count = cfg.count || 12;
-    var section = cfg.section;
-    var values = [];
-    var i, value;
-
-    for (i = 0; i < count; ++i) {
-      value = MONTHS[Math.ceil(i) % 12];
-      values.push(value.substring(0, section));
-    }
-
-    return values;
-  }
-
-  const labels = months({ count: 12 });
 
   let noveVrednosti = {...vrednosti};
 
@@ -114,110 +82,61 @@ const LineGraph = (props) => {
   labelArray.push(initialTime);
   labelArray.push(new Date(Date.now()));
 
-  // let tempTime = initialTime;
-  // tempTime.setDate(tempTime.getDate() - 1);
-  // for (let i = 0; i < 2; i++) {
-  //   tempTime.setHours(tempTime.getHours() + 1);
-  //   console.log(tempTime.getHours());
-  //   let formattedTempTime = tempTime.toISOString();
-  //   console.log(formattedTempTime);
-  //   labelArray.push(tempTime);
-  //   // if (i < 10)
-  //   //   labelArray.push(formattedTempTime.replace("T00:00:00", "T0" + i + ":00:00"));
-  //   // else labelArray.push(formattedTempTime.replace("T00:00:00", "T" + i + ":00:00"));
-  // }
-  // break;
-  // case "Mesec":
-  //   for (let i = 1; i < 32; i++) {
-  //     let tempTime = initialTime;
-  //     tempTime.setDate(tempTime.getMonth() -1);
-  //     let formattedTempTime = tempTime.toISOString();
-  //     if (i < 10)
-  //       labelArray.push(formattedTempTime.replace("T00:00:00", "T0" + i + ":00:00"));
-  //     else labelArray.push(formattedTempTime.replace("T00:00:00", "T" + i + ":00:00"));
-  //   }
-  //   break;
-  // }
-
   const dataSetovi = [];
   let index = 0;
-  //debugger;
   function ucitajVrednosti() {
-    var ctx = document.getElementById("lineGraph").getContext("2d");
+    //var ctx = document.getElementById("lineGraph").getContext("2d");
 
-
-
-    
     for (var key in noveVrednosti) {
-      let rnd1 = Math.random() * 255;
-      //debugger;
       let broj = noveVrednosti[key].length - 1;
       let dodatnaTacka = { ...noveVrednosti[key][broj] };
       let novoX = new Date(Date.now()).toISOString();
       dodatnaTacka.x = novoX;
       noveVrednosti[key].push(dodatnaTacka);
 
-      let a = niceColors[index];
-      let b = niceColorsOpacity[index];
-      const gradientBg = ctx.createLinearGradient(0, 0, 0, 500);
-      gradientBg.addColorStop(0, a);
-      gradientBg.addColorStop(0.25, b);
-      //gradientBg.addColorStop(0.5, '#00000000');
-
+      // ! Re-add this if you figure out a method to get line height:
+      // let a = niceColors[index];
+      // let b = niceColorsOpacity[index];
+      // const gradientBg = ctx.createLinearGradient(0, 0, 0, 500);
+      // gradientBg.addColorStop(0, a);
+      // gradientBg.addColorStop(0.25, b);
 
       dataSetovi.push({
         label: key,
         data: noveVrednosti[key],
-        fill: true,
+        fill: '+1',
         borderColor: niceColors[index],
-        backgroundColor: gradientBg,
-        pointBackgroundColor: gradientBg,
-        //strokeColor: niceColors[index],
-        //pointColor: niceColors[index],
-        //pointStrokeColor: "#202b33",
-        //pointHighlightStroke: "rgba(225,225,225,0.9)",
+        backgroundColor: niceColorsOpacity[index],
+        pointBackgroundColor: niceColors[index],
         tension: 0.1,
       });
-
       index++;
     }
   }
-  //ucitajVrednosti();
-  var options = {
-    // pointDotRadius : 6,
-    // pointDotStrokeWidth : 2,
-    // datasetStrokeWidth : 3,
-    // scaleShowVerticalLines: false,
-    // scaleGridLineWidth : 2,
-    // scaleShowGridLines : true,
-    // scaleGridLineColor : "rgba(225, 255, 255, 0.02)",
-    // scaleOverride: true,
-    // scaleSteps: 9,
-    // scaleStepWidth: 500,
-    // scaleStartValue: 0,
+
+  const data = {
+    labels: labelArray,
+    datasets: dataSetovi,
+  };
+
+  const options = {
     fill: true,
     animation: true,
     responsive: true,
     interaction: {
       mode: "index",
-      intersect: true,
+      intersect: false,
     },
     scales: {
       x: {
         type: "time",
         time: {
-           //min: initialTime,
-           //max: new Date(Date.now()),
           unit: raspon || "hour",
         },
       },
     },
   };
 
-  const data = {
-    labels: labelArray,
-    datasets: dataSetovi,
-  };
   const config = {
     type: "line",
     data: data,
