2cd5516b3449c3f58679363280b5304749aa28f7
Djordje Zivic <djzivic@gmail.com>
2022-02-07 16:31:00 +0100

    bootstrap transition for sidebar

--- inventar/src/components/Inventar.js
+++ inventar/src/components/Inventar.js
@@ -1,25 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import Navigation from "./Nav/Navigation";
 import Sidebar from "./Nav/Sidebar";
 import Main from "./Main";
 
-
 const Inventar = () => {
- const [collapse, setCollapse] = useState(false);
- const toggleCollapse = () => {
-  setCollapse(!collapse);
- }
+  const [collapse, setCollapse] = useState(false);
+  const toggleCollapse = () => {
+    setCollapse(!collapse);
+  };
 
-//TODO: Redux 
-    return (
-        <div className="App">
-          <Navigation toggleCollapse={() => toggleCollapse()}/>
-          <div className="flex row flex-nowrap fullHeight">
-           {!collapse && <Sidebar />}
-            <Main />
-          </div>
-        </div>
-    )
-}
+  //TODO: Redux
+  return (
+    <div className="App">
+      <Navigation toggleCollapse={() => toggleCollapse()} />
+      <div className="flex row flex-nowrap fullHeight">
+        <Sidebar isCollapsed={collapse} />
+        <Main/>
+      </div>
+    </div>
+  );
+};
 
 export default Inventar;
